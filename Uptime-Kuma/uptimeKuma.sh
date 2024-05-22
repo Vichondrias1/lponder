a2c8dd89e7a98f86f426f096927da18983c688ae
Vichondrias1 <39474726+Vichondrias1@users.noreply.github.com>
2024-05-22 10:07:33 +0800

    ambot

--- Uptime-Kuma/uptimeKuma.sh
+++ Uptime-Kuma/uptimeKuma.sh
@@ -5,10 +5,11 @@
 
 sudo mkdir uptime-kuma
 
+sleep 3
 #download the yml file to the current directory
-curl -o /uptime-kuma/docker-compose.yml https://raw.githubusercontent.com/Vichondrias1/lponder/main/docker-compose.yml
+curl -o uptime-kuma/docker-compose.yml https://raw.githubusercontent.com/Vichondrias1/lponder/main/docker-compose.yml
 #curl -O https://raw.githubusercontent.com/Vichondrias1/lponder/main/docker-compose.yml
 
 #execute the yml file
-docker-compose -f /uptime-kuma/docker-compose.yml up
+docker-compose -f uptime-kuma/docker-compose.yml up
 #docker compose up -d
